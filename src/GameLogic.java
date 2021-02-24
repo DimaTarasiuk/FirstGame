f96bf640ea44ebec99690dbb493f1ae660ce32e7
DimaTarasiuk <mr.tarasiuk@gmail.com>
2021-02-25 00:55:15 +0200

    changed game logig

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,55 +1,56 @@
-import java.io.IOException;
 import java.util.Random;
 
 public class GameLogic {
     public static String[] figures = {"stone", "paper", "noznytsi"};
     public static boolean isGameRun = true;
-    public static String playersChoose = "stone";
+    public static String playersChoose = "paper";
+    public static String botChoose = "paper"; //randSymbol();
 
-    public static void playerSymbolChooser(){
-        //todo
-        //make realtime choose of symbol
-    }
 
-    public static String randSymbol(){
+    public static String randSymbol() {
         Random random = new Random();
-        String symbol = figures[random.nextInt(figures.length)];
-        return symbol;
+        return figures[random.nextInt(figures.length)];
     }
 
-    public static void startGame(){
-        while (isGameRun){
+    public static void startGame() {
 
-            if(randSymbol() == playersChoose){
-                System.out.println(randSymbol() + " " + playersChoose );
+
+        while (isGameRun) {
+
+            if (botChoose.equalsIgnoreCase(playersChoose)) {
+                System.out.println(botChoose + " " + playersChoose);
                 System.out.println("Nichiya \n"); // new line
-                // todo here is some bug. when different symbols can be nichia
+                botChoose = randSymbol();
 
-            }else if(randSymbol()== "stone" || playersChoose == "paper"){
-                System.out.println("you: "+ playersChoose + " II: stone");
+            } else if (botChoose.equalsIgnoreCase("stone") || playersChoose.equalsIgnoreCase("paper")) {
+                System.out.println("you: " + playersChoose + " bot: " + botChoose);
                 System.out.println("You won");
                 isGameRun = false;
 
-            }else if(randSymbol() == "stone" || playersChoose == "noznytsi"){
-                System.out.println("you: "+ playersChoose + " II: stone");
+            } else if (botChoose.equalsIgnoreCase("stone") || playersChoose.equalsIgnoreCase("noznytsi")) {
+                System.out.println("you: " + playersChoose + " bot: " + botChoose);
                 System.out.println("you lose ");
                 isGameRun = false;
 
-            }else if(randSymbol() == "paper" || playersChoose == "stone"){
-                System.out.println("you: "+ playersChoose + " II: paper");
+            } else if (botChoose.equalsIgnoreCase("paper") || playersChoose.equalsIgnoreCase("stone")) {
+                System.out.println("you: " + playersChoose + " bot: " + botChoose);
                 System.out.println("you lose");
                 isGameRun = false;
 
-            }else if(randSymbol() == "noznytsi" || playersChoose == "paper"){
-                System.out.println("you: "+ playersChoose + " " + " II: noznytsi");
+            } else if (botChoose.equalsIgnoreCase("noznytsi") || playersChoose.equalsIgnoreCase("paper")) {
+                System.out.println("you: " + playersChoose + "bot " + botChoose);
                 System.out.println("you lose");
                 isGameRun = false;
-            }
-            else{
-                System.out.println("no such combination, rand symbol = " + randSymbol());
+            } else if (botChoose.equalsIgnoreCase("noznytsi") || playersChoose.equalsIgnoreCase("stone")) {
+                System.out.println("You:" + playersChoose + "bot : " + botChoose);
+                System.out.println("You won");
+            } else {
+                System.out.println("no such combination, rand symbol = " + botChoose);
             }
 
         }
     }
 
 }
+
+
